ad584012218c64f16bb206406ef7d4c9b32c9512
Rameez <rameebappi8@gmail.com>
2021-02-23 12:34:29 +0530

    Schema added

---
+++ models/user-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+module.exports = mongoose.model('User ', Schema({
+    _id: Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    admin_id: {
+        required: true,
+        type: Schema.Types.ObjectId
+    }
+}))
